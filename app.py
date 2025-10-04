5acc9e5c2d8115cbfbcc77b012ab8df503d89076
HuoJia <hjia@curietech.net>
2025-10-04 17:22:15 +0800

    feat: update env variable name

--- app.py
+++ app.py
@@ -323,7 +323,7 @@ def download_qrcode(file_id):
 
 
 if __name__ == '__main__':
-    debug = os.getenv('FLASK_DEBUG', 'False').lower() == 'true'
-    host = os.getenv('FLASK_HOST', '0.0.0.0')
-    port = int(os.getenv('FLASK_PORT', '5000'))
+    debug = os.getenv('DEBUG', 'False').lower() == 'true'
+    host = os.getenv('HOST', '0.0.0.0')
+    port = int(os.getenv('PORT', '5000'))
     app.run(debug=debug, host=host, port=port)
